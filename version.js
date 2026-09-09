daf5821c74e2f091fbe4653c85dea8b868b4d5cf
juvsnt <juvsnet@gmail.com>
2026-09-09 17:14:24 -0300

    Corrige dicas da versão v1.2.1

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 // Versionamento: vMAJOR.MINOR.PATCH.
 // MAJOR = mudança estrutural; MINOR = recurso novo; PATCH = correção ou ajuste pequeno.
-const FORCA_VERSION = 'v1.2.0';
+const FORCA_VERSION = 'v1.2.1';
